8e2b4e9b09d19e410ba8d8047c2891f2c98e5c6d
Sricharan Kontham <58589653+charankontham@users.noreply.github.com>
2024-09-15 04:25:18 -0400

    Initial push

---
+++ src/main/java/com/ecs/ecs_service_registry/EcsServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsServiceRegistryApplication.class, args);
+	}
+
+}
