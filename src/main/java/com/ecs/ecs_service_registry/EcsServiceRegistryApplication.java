91e796603df8e3a5a1ff47d29e8bc0617166f3ad
= <=>
2024-11-02 18:24:26 -0400

    Updates to the Service-registry

--- src/main/java/com/ecs/ecs_service_registry/EcsServiceRegistryApplication.java
+++ src/main/java/com/ecs/ecs_service_registry/EcsServiceRegistryApplication.java
@@ -2,12 +2,12 @@ package com.ecs.ecs_service_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EcsServiceRegistryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EcsServiceRegistryApplication.class, args);
 	}
-
 }
